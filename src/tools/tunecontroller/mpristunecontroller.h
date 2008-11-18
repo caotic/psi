86c6276a378bfa26059c680f83696384215a30eb
Matthieu Volat <vmazhe@gmail.com>
2008-11-18 16:16:32 +0100

    Added Matthieu Volat's mpris tune controller

---
+++ src/tools/tunecontroller/mpristunecontroller.h
@@ -0,0 +1,58 @@
+/*
+ * mpristunecontroller.h
+ * Copyright (C) 2008 Matthieu Volat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef MPRISTUNECONTROLLER_H
+#define MPRISTUNECONTROLLER_H
+
+#include <QDBusConnection>
+#include <QString>
+
+#include "tunecontrollerinterface.h"
+#include "tune.h"
+
+struct PlayerStatus
+{
+	int i1;
+	int i2;
+	int i3;
+	int i4;
+};
+
+Q_DECLARE_METATYPE(PlayerStatus)
+
+class MPRISTuneController : public TuneController
+{
+	Q_OBJECT
+
+public:
+	MPRISTuneController(const QString &player);
+
+	Tune currentTune();
+
+protected slots:
+	void playerStatusChanged(const PlayerStatus& ps);
+	void playerTrackChanged(QVariantMap map);
+
+private:
+	Tune currentTune_;
+	QString service_;
+};
+
+#endif
